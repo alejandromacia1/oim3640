23717699188a1798d53ec639974652e6ae0f99c3
alejandromacia1 <amacia1@babson.edu>
2026-01-29 10:58:43 -0500

    Commit

---
+++ code/calc.py
@@ -0,0 +1,7 @@
+print(2 + 4) #in python you have to put print for the output to appear. 
+
+print (10 - 3) #minus signs performs subtraction
+
+print(3 * 3) #a single asterisk performs multiplication
+
+print (3 ** 8) #double asterisk performs power. Hence, in this case it would be to the power of 8. 
