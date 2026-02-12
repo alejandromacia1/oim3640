0d1ce7691994847161d46eec6be59d939c2cc9b6
alejandromacia1 <amacia1@babson.edu>
2026-02-12 11:05:54 -0500

    Session 08 Exercise: if and ifelse

---
+++ code/sp08_demo.py
@@ -0,0 +1,31 @@
+
+#Classifying a person based on their age and printing result. 
+#age = 21
+
+#if age < 18:
+    #print("Teenager")
+#else:
+    #print("Adult")
+
+
+#age = int(input('What is your age?   >>'))
+
+#if age < 18: 
+    #print('Teenager')
+#elif age > 65:
+    #print('You are too old')
+#else:
+    #print('Adult')
+
+score = 91
+
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+else: 
+    print('F') #order matters - first matching condition wins. 
+    #important: each branch must be indented.
+
