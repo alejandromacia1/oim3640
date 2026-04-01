46cb06b392d773f004be13b2ee4bf4d8e01b58f6
alejandromacia1 <amacia1@babson.edu>
2026-04-01 19:10:34 -0400

    Session 17

---
+++ code/s17_anagram.py
@@ -0,0 +1,29 @@
+
+"""
+for word in word_list:
+    for word in rest_list:
+        check whether they are anagrams
+
+        
+
+
+aab # store this word somewhere
+deltas #
+
+subquesiton: how to check two words are anagram
+
+subquesiton: how to store the angrams
+
+xyz # ...
+desalt
+aba
+
+
+d = {
+'aab': [aab],
+'adelst': [deltas],
+'xyz': [xyz],
+}
+
+desalt -> 'adelst' -> check whether it exists in d -> d['adelst'].append('desalt')
+"""
