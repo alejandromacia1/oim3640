c340a042d64165ab09db8ff8ca5cd60d398d5569
alejandromacia1 <amacia1@babson.edu>
2026-04-06 19:49:01 -0400

    Session 18

--- code/s18_api_demo.py
+++ code/s18_api_demo.py
@@ -1,26 +1,40 @@
 
 import requests
 from pprint import pprint
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
+url = (f'https://api.openweathermap.org/data/2.5/weather?q=Boston&appid={API_KEY}$units=imperial')
 
 # response = requests.get("https://oim.108122.xyz/words/random")
 
 # print(response.json())
 
-response = requests.get(
-    "https://oim.108122.xyz/mass",
-    headers={"X-Token": "zhizhi"},
-)
-data = response.json()
+#response = requests.get(
+    #"https://oim.108122.xyz/mass",
+    #headers={"X-Token": "alejandroalejandro"},
+#)
+#data = response.json()
 
 # print(data['name'])       # 'Massachusetts'
 # print(data['governor'])   # 'Maura Healey'
 
-print(len(data))
-print(data.keys())
+#print(len(data))
+#print(data.keys())
 
-towns = data["data"]
-print(type(towns))  # do this for explore the data structure
+#towns = data["data"]
+#print(type(towns))  # do this for explore the data structure
 
 # pprint(towns)
-print(len(towns))
+# print(len(towns))
+#pprint(towns[0])  # print the first town
+
+#requests.post(
+    #'https://oim.108122.xyz/message',
+    #json={'message': 'Hello from Alejandro!'},
+    #headers={'X-Token': 'alejandroalejandro'}
+#)
+
