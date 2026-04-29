8a37234c62cf5a2de4b4ed27484110b3f72a183c
alejandromacia1 <amacia1@babson.edu>
2026-04-28 21:40:31 -0400

    Mini Project 3 progress

---
+++ projects/miniproject_3/main.py
@@ -0,0 +1,18 @@
+
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAPBOX_TOKEN = os.getenv("MAPBOX_TOKEN")
+MBTA_API_KEY = os.getenv("MBTA_API_KEY")
+
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAPBOX_TOKEN = os.getenv("MAPBOX_TOKEN")
+MBTA_API_KEY = os.getenv("MBTA_API_KEY")
